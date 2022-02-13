e02d312ad0a736b8a5931ef3c4f925176b5ad537
Martha Bulaccio <maralibu@hotmail.com>
2022-02-13 17:54:28 -0300

    third commit = Create ItemCount

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 
 function App() {
+
+  const addtoCart = (count) => {
+    console.log(`Se agregan al carrito ${count} productos`);
+
+  };
   return (
     <div className="App">
-      <NavBar/>
-      <ItemListContainer greeting={"HOLA MUNDO"}/>     
+      <NavBar />
+      <ItemListContainer greeting={"BIENVENIDO A BISTREA SHOP"} />
+      <ItemCount stock={10} initial={0} addtoCart={addtoCart} />
     </div>
   );
+
 }
 
 export default App;
+
